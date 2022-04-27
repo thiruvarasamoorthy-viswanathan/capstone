c1ca57f877615dee9b1285b4351fd570fd506a9b
Thiruvarasamoorthy Viswanathan <vtm1983@gmail.com>
2022-04-27 08:38:53 -0500

    update app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    html = f"<h2>Hello World, my name is Thiruvarasamoorthy Viswanathan</h2>"
+    html = f"<h3>Hello World, my name is Thiruvarasamoorthy Viswanathan</h3>"
     return html.format(format)
 
 if __name__ == "__main__":
