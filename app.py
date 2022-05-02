06b459116e46459c2afa527f34ebcf3dc8d13a2c
Thiruvarasamoorthy Viswanathan <vtm1983@gmail.com>
2022-05-02 12:40:35 -0500

    Update

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    html = f"Hello World, my name is Thiruvarasamoorthy Viswanathan"
+    html = f"<h1><p>Hello World, my name is Thiruvarasamoorthy Viswanathan</h1></p>"
     return html.format(format)
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)  # specify port=80
+    app.run(host='0.0.0.0', port=80, debug=True)  # specify port=80    
