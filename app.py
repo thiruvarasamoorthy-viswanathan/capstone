50db3d9b7eae3d24fc4f48a11cdd8a9e78400703
Thiruvarasamoorthy Viswanathan <vtm1983@gmail.com>
2022-05-04 16:54:26 -0500

    Fix Lint failure - update app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True,90)  # specify port=80
+    app.run(host='0.0.0.0', port=80, debug=True)  # specify port=80
